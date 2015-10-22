d1d9658f83f6f5d7d5a1456f719fbe15378f0cd5
rsussan <rsussa1@students.towson.edu>
2015-10-21 20:41:09 -0400

    Imported all files previously made

---
+++ src/edu/towson/cosc436/rsussa1/Decorator_Pattern/Basic_Reciept.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.towson.cosc436.rsussa1.Decorator_Pattern;
+
+/**
+ *
+ * @author rsussa1
+ */
+public class Basic_Reciept {
+    public void create(){
+        
+    }
+    
+    public void printReciept(){
+        
+    }
+    
+    public void addTaxMethod(){
+        
+    }
+}
---
+++ src/edu/towson/cosc436/rsussa1/Decorator_Pattern/Decorator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.towson.cosc436.rsussa1.Decorator_Pattern;
+
+/**
+ *
+ * @author rsussa1
+ */
+abstract class Decorator {
+    private Component trailer; // points to another decorator or the reciept
+    
+    public void create(Component c){
+        
+    }
+    
+    //this method calls the print reciept method of the next portion of the reciept
+    public void callTrailer(){
+        
+    }
+}
